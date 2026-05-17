cb350d3ca78d4c9822dd91f3c4a53458d136dfbf
Nnamdi <cavennnamdi@gmail.com>
2026-05-17 17:03:51 +0100

    CHIN-CHIN

---
+++ Assignments/easterEggs/chin-chin/HeadsOrTails.js
@@ -0,0 +1,9 @@
+let toss = Math.floor(Math.random() * 2);
+
+if (toss == 0){
+    console.log("Heads")
+}
+else{
+console.log("Tails")
+}
+console.log(toss)
---
+++ Assignments/easterEggs/chin-chin/SubtractingRandom.js
@@ -0,0 +1,6 @@
+let numberOne = Math.floor(Math.random() * 20);
+let numberTwo = Math.floor(Math.random() * numberOne);
+
+let subtracting = numberOne - numberTwo;
+
+console.log(`${numberOne} - ${numberTwo} = ${subtracting}`);
